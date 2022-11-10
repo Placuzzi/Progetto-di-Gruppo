f7e3848afcbbc3b1663931f68bca805856852f00
Lorenzo Belletti <LORENZO.BELLETTI@local.ittcesena.it>
2022-11-10 09:20:35 +0100

    Versione Brano completa

---
+++ Esercizio_Git_gruppo/Brano.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Git_gruppo
+{
+    internal class Brano
+    {
+    }
+}
