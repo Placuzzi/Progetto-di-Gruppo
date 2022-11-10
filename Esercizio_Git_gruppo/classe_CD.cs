bb58500f0a8fe71d21bf6f7bbc95cdb170a794e3
Alberto Vincenzi <ALBERTO.VINCENZI@local.ittcesena.it>
2022-11-10 09:46:08 +0100

    commit del master

---
+++ Esercizio_Git_gruppo/classe_CD.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Esercizio_Git_gruppo
+{
+    internal class classe_CD
+    {
+
+
+        List<String> brano = new List<String>();
+
+
+      
+
+
+
+
+        public string costrutore(string Titolo)
+        {
+
+
+            return Titolo;
+        }
+
+
+        public string getTitolo(string titolo)
+        {
+
+
+
+
+            return titolo;
+        }
+
+
+
+        public string getAutore(string autore)
+        {
+
+
+
+
+            return autore;
+        }
+
+
+
+        public string setTitolo(string titolo)
+        {
+
+
+
+            return titolo;
+        }
+
+
+
+        public string setAutore(string autore)
+        {
+
+
+
+            return autore;
+
+        }
+
+
+
+        public double Durata(double t)
+        {
+
+
+
+
+            return t;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
